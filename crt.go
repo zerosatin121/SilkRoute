e3b556d8b39a74ce37b0dfc3b56b2377b59e5cad
zerosatin121 <mathu.muchoki@gmail.com>
2025-07-27 05:17:15 +0300

    read: response body

--- crt.go
+++ crt.go
@@ -18,4 +18,15 @@ func GetSubdomains(domain string)([]string, error){
 	client := &http.Client{}
 
 	resp, err := client.Get(url)
+	if err !=nil{
+		return nil,fmt.Errorf("http error: %w" , err)
+	} 
+	// close response body to avoid leakage.....
+	defer resp.Body.Close()
+
+	body , err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil ,fmt.Errorf("read  error :%w" , err)
+	}
+
 }
